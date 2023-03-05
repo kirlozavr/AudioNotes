5f1f32aed293f7b07c5b4c91056d790cee4a6326
kirlozavr <kirill.protchenko@mail.ru>
2023-03-05 19:34:53 +1000

    Класс с константами ключей для передачи информации между Activity

---
+++ app/src/main/java/com/example/audionotes/common/ConstValues.java
@@ -0,0 +1,7 @@
+package com.example.audionotes.common;
+
+public class ConstValues {
+    public static final String KEY_FILE_PATH = "filePath";
+    public static final String KEY_FILE_NAME = "fileName";
+    public static final String KEY_TIMING_AUDIO = "timingAudio";
+}
