5d6bdfb69cf536abe422902c1120b10fb68bef98
kirlozavr <kirill.protchenko@mail.ru>
2023-03-08 20:28:35 +1000

    Были внесены изменения связанные с отображением прогресса и небольшие изменения для корректной работы

--- app/src/main/java/com/example/audionotes/service/MediaPlayService.java
+++ app/src/main/java/com/example/audionotes/service/MediaPlayService.java
@@ -2,56 +2,70 @@ package com.example.audionotes.service;
 
 import android.media.MediaPlayer;
 import android.os.Handler;
+import android.util.Log;
+
 import java.io.IOException;
 
 public class MediaPlayService {
 
     private MediaPlayer mediaPlayer;
     private boolean isPlaying = false;
-    private Handler handler;
-    private Runnable run;
-    private OnProgress onProgress;
-
-    public MediaPlayService(OnProgress onProgress) {
-        this.onProgress = onProgress;
-    }
-
-    public void start(String fileAbsolutePath) {
-        try {
-            if (isPlaying) {
-                stop();
-            }
-            handler = new Handler();
-            run = new Runnable() {
+    private String fileAbsolutePath;
+    private MediaPlayer.OnCompletionListener onCompletionListener =
+            new MediaPlayer.OnCompletionListener() {
                 @Override
-                public void run() {
-                    if (mediaPlayer != null) {
-                        onProgress.progress(mediaPlayer.getCurrentPosition());
-                        handler.postDelayed(this, 100);
-                    }
+                public void onCompletion(MediaPlayer mediaPlayer) {
+                    isPlaying = false;
+                    mediaPlayer.stop();
+                    mediaPlayer.reset();
                 }
             };
-            handler.postDelayed(run, 0);
-            isPlaying = true;
-            mediaPlayer = new MediaPlayer();
+
+    public MediaPlayService() {
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer.setOnCompletionListener(onCompletionListener);
+    }
+
+    public void setFileAbsolutePath(String fileAbsolutePath) {
+        this.fileAbsolutePath = fileAbsolutePath;
+        try {
             mediaPlayer.setDataSource(fileAbsolutePath);
             mediaPlayer.prepare();
-            mediaPlayer.start();
-
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
+    public void start() {
+
+        if (isPlaying) {
+            stop();
+        }
+
+        if (fileAbsolutePath.equals("") || fileAbsolutePath.length() != 0) {
+            isPlaying = true;
+
+            mediaPlayer.start();
+
+        }
+    }
+
+    public int getProgress() {
+        return mediaPlayer.getCurrentPosition();
+    }
+
+    public boolean isPlaying() {
+        return isPlaying;
+    }
+
     public void stop() {
         isPlaying = false;
-        if(mediaPlayer != null){
-            mediaPlayer.stop();
-        }
-        mediaPlayer = null;
+        mediaPlayer.stop();
+        mediaPlayer.reset();
     }
 
     public int length() {
+        Log.i("папа", String.valueOf(mediaPlayer.getDuration()));
         return mediaPlayer.getDuration();
     }
 }
